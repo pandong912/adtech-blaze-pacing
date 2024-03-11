86b36aa3c3aacd35fbaf260015943a9c627f9859
RanRan <ran.ran@hotstar.com>
2024-03-11 20:09:17 +0800

    fix debug controller

--- ad-allocation-planner/src/main/java/com/hotstar/adtech/blaze/allocation/planner/controller/DebugController.java
+++ ad-allocation-planner/src/main/java/com/hotstar/adtech/blaze/allocation/planner/controller/DebugController.java
@@ -3,15 +3,18 @@ package com.hotstar.adtech.blaze.allocation.planner.controller;
 import com.hotstar.adtech.blaze.allocation.planner.common.admodel.AdModel;
 import com.hotstar.adtech.blaze.allocation.planner.ingester.DataLoader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("debug")
+@Profile("!sim && !worker")
 @RequiredArgsConstructor
 public class DebugController {
   private final DataLoader dataLoader;
+
 
   @GetMapping("/ad-model")
   public AdModel getAdModel() {

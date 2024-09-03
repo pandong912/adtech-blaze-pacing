10ea81ad1c8da06e3bafff1d544fb2b216fa4445
RanRan <ran.ran@hotstar.com>
2024-09-03 18:06:40 +0800

    remove tags

--- ad-allocation-planner/src/main/java/com/hotstar/adtech/blaze/allocation/planner/config/MetricRegistryConfig.java
+++ ad-allocation-planner/src/main/java/com/hotstar/adtech/blaze/allocation/planner/config/MetricRegistryConfig.java
@@ -2,7 +2,10 @@ package com.hotstar.adtech.blaze.allocation.planner.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import java.util.Collections;
+import java.util.function.Function;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,7 @@ public class MetricRegistryConfig {
 
   @Bean
   public TimedAspect timedAspect(MeterRegistry registry) {
-    return new TimedAspect(registry);
+    return new TimedAspect(registry,
+      (Function<ProceedingJoinPoint, Iterable<Tag>>) (pjp) -> Collections.emptyList());
   }
 }
